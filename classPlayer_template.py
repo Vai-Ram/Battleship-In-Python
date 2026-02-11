d27741e2da972a9512bde34defc710735603e823
kushal-in <kushalvurukonda@gmail.com>
2026-02-11 23:49:18 +0530

    player_class

--- classPlayer_template.py
+++ classPlayer_template.py
@@ -1,3 +1,7 @@
+from classBoard_template import Board
+from classShip_template import Ship
+import random
+
 class Player:
     def __init__(self, name):
         """
@@ -6,7 +10,9 @@ class Player:
         2. Create self.board = Board() (This is the player's own fleet).
         3. Create self.shots_fired = set() (Tracks coordinates this player has attacked).
         """
-        pass
+        self.name = name
+        self.board = Board()
+        self.shots_fired = set()
 
     def random_place_ships(self):
         """
@@ -20,7 +26,21 @@ class Player:
              - If True: Break retry loop, move to next ship size.
              - If False: Continue retry loop (position was invalid).
         """
-        pass
+        fleet = {
+            "Carrier": 5, "Battleship": 4, "Submarine": 3, "Cruiser": 3, "Destroyer": 2
+        }
+        alpha={0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J"}
+        for ship in fleet:
+            while True:
+                row = random.randint(0, 9)
+                col = random.randint(0, 9)
+                orientation = random.choice(['H', 'V'])
+                ship_to_place = Ship(ship, fleet[ship])
+
+                if self.board.place_ship(ship_to_place, row, col, orientation):
+                    print(
+                        f"{ship} successfully placed at {alpha[row]+str(col+1)} with orientation {orientation}")
+                    break
 
     def manual_place_ships(self):
         """
@@ -36,7 +56,44 @@ class Player:
              - If True: Print success message, break validation loop.
              - If False: Print error ("Invalid/Overlap"), repeat validation loop.
         """
-        pass
+        fleet = {
+            "Carrier": 5, "Battleship": 4, "Submarine": 3, "Cruiser": 3, "Destroyer": 2
+        }
+
+        for ship in fleet:
+            self.board.display()
+
+            while True:
+                coordinate = input(
+                    f"Start coordinate for {ship} (Eg. C4): ").strip()
+                orientation = input("Orientation (H/V): ").strip()
+
+                if len(orientation) == 1 and orientation.lower() in ('h', 'v'):
+                    if 2 <= len(coordinate) <= 3:
+                        row = coordinate[0]
+                        col = coordinate[1:]
+
+                        if ('a' <= row.lower() <= 'j') and col.isdigit():
+                            row = ord(row.lower()) - ord('a')
+                            col = int(col) - 1
+
+                            if 0 <= col <= 9:
+                                ship_to_place = Ship(ship, fleet[ship])
+
+                                if self.board.place_ship(ship_to_place, row, col, orientation):
+                                    print(
+                                        f"{ship} successfully placed at {coordinate} with orientation {orientation}")
+                                    break
+                                else:
+                                    print("Invalid ship placement")
+                            else:
+                                print("Invalid input")
+                        else:
+                            print("Invalid input")
+                    else:
+                        print("Invalid input")
+                else:
+                    print("Invalid input")
 
     def attack(self, enemy_board):
         """
@@ -52,4 +109,28 @@ class Player:
         4. Print the result (e.g., "Hit!", "Miss!", "Sunk!").
         5. Return the result string.
         """
-        pass
+        while True:
+            coordinate = input(
+                f"{self.name}, enter target coordinate: ").strip()
+            if 2 <= len(coordinate) <= 3:
+                row = coordinate[0]
+                col = coordinate[1:]
+
+                if ('a' <= row.lower() <= 'j') and col.isdigit():
+                    row = ord(row.lower()) - ord('a')
+                    col = int(col) - 1
+
+                    if 0 <= col <= 9:
+                        if (row, col) in self.shots_fired:
+                            print("You already shot there!")
+                        else:
+                            result = enemy_board.receive_attack(row, col)
+                            self.shots_fired.add((row, col))
+                            print(result)
+                            return result
+                    else:
+                        print("Invalid input")
+                else:
+                    print("Invalid input")
+            else:
+                print("Invalid input")
